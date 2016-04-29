63e23129d1bf89023691c4f420a082c0e07f7e16
Jimmy Quach <jgquach@ucsd.edu>
2016-04-28 18:20:06 -0700

    this is the code with the db using nexus 5x, I followed some firebase tutorial online, throws a fatal error when adding stuff to the db, I'll try and fix soon

---
+++ app/src/main/java/com/example/boredguy88/test2/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.boredguy88.test2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mTextFieldCondition;
+    Button cats;
+    Button kats;
+    Firebase mRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mTextFieldCondition = (TextView)findViewById(R.id.textView);
+        cats = (Button)findViewById(R.id.button);
+        kats = (Button)findViewById(R.id.button2);
+
+        mRef = new Firebase("https://blinding-heat-8007.firebaseio.com");
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                mTextFieldCondition.setText(text);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
+
+        cats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRef.setValue("cats");
+            }
+        });
+
+        kats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRef.setValue("kats");
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
